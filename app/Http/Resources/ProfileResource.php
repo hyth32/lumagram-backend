1f9174e7d3c06505949e52863ec736341403304d
Ivan Goryaev <5474n.sage@gmail.com>
2025-04-13 20:44:26 +0300

    добавил изображение в сериализацию профиля

--- app/Http/Resources/ProfileResource.php
+++ app/Http/Resources/ProfileResource.php
@@ -16,6 +16,7 @@ class ProfileResource extends JsonResource
             'description' => $this->description,
             'activityCategory' => $this->activity_category,
             'isPublic' => $this->is_public,
+            'image' => ImageResource::make($this->avatar),
         ];
     }
 }
