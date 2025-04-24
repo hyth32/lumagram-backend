340e1e0618055a28f46fe217bc9b4401bf207320
Ivan Goryaev <5474n.sage@gmail.com>
2025-04-24 18:39:05 +0300

    попытка отключить csrf

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,6 +23,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
